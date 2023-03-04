160762a4d651aa20c61a380a630cf44fc7c854d9
MaxDLink <max.durham.link@gmail.com>
2023-03-04 16:27:55 -0600

    commented prog2test.cpp

--- prog2test.cpp
+++ prog2test.cpp
@@ -7,47 +7,47 @@ using namespace std;
 
 int main()
 {
-  ifstream ifile("proog2input.txt");
-  int n, e, c;
+  ifstream ifile("proog2input.txt"); //opens input.txt file to read input 
+  int n, e, c; //creates variables to store input 
 
-  ifile >> n >> e >> c;
-  MyGraph g(n);
+  ifile >> n >> e >> c; //reads in input values to variables 
+  MyGraph g(n); //creates graph object 
 
-  for (int i = 0; i < e; i++)
+  for (int i = 0; i < e; i++) //loops through all edges in the graph 
   {
-    int s, t;
+    int s, t; 
     float w;
-    ifile >> s >> t >> w;
-    g.AddEdge(s, t, w);
+    ifile >> s >> t >> w; //stores int values in s and t and float weight values in w 
+    g.AddEdge(s, t, w); //passes these values to AddEdge function to see if edge needs to be added to graph or not 
   }
 
-  vector<int> p, q;
-  for (int i = 0; i < c; i++)
+  vector<int> p, q; //create vectors p and q 
+  for (int i = 0; i < c; i++) //c represents vertex pairs for which the highest capacity path needs to be computed 
   {
-    int s, t;
-    ifile >> s >> t;
-    p.push_back(s);
-    q.push_back(t);
+    int s, t; 
+    ifile >> s >> t; //start vertex and end vertex assigned to s and t respectively 
+    p.push_back(s); //p vector gets start vertex 
+    q.push_back(t); //q vector gets end vertex 
   }
 
-  ifile.close();
+  ifile.close(); //close file 
 
-  vector<pair<vector<int>, float> > resarray;
+  vector<pair<vector<int>, float> > resarray; //resarray stores results after calling HW2Prog method below 
 
   // the timer will time this for loop
 
-  for (int i = 0; i < p.size(); i++)
+  for (int i = 0; i < p.size(); i++) //loop through p vector 
   {
-    pair<vector<int>, float> res = g.HW2Prog(p[i], q[i], true);
-    resarray.push_back(res);
+    pair<vector<int>, float> res = g.HW2Prog(p[i], q[i], true); //calls HW2Prog method and stores results in res 
+    resarray.push_back(res); //pushes res into resarray 
   }
 
-  for (int i = 0; i < p.size(); i++)
+  for (int i = 0; i < p.size(); i++)//loop through p vector pair 
   {
-    cout << p[i] << " -> " << q[i] << " : Path : ";
-    for (int j = 0; j < resarray[i].first.size(); j++)
-      cout << resarray[i].first[j] << "  ";
-      cout << "  capacity : " << resarray[i].second << endl;
+    cout << p[i] << " -> " << q[i] << " : Path : "; //print the start vertex, end vertex
+    for (int j = 0; j < resarray[i].first.size(); j++) //loop through resarray 
+      cout << resarray[i].first[j] << "  "; //print the path 
+      cout << "  capacity : " << resarray[i].second << endl; //print the capacity 
   }
 
   return (0);
