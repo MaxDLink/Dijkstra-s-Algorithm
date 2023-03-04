2548166f88f979cbe21d0ad1b7a8c38b132ffe99
MaxDLink <max.durham.link@gmail.com>
2023-03-04 15:21:56 -0600

    added #include <unordered_map>, added unordered_map<int, vector<pair<int, float>>> adjList private data member. Added addVertex(i) function that only adds a vertex if it is not in the adjList map.

--- MyGraph.h
+++ MyGraph.h
@@ -1,12 +1,26 @@
 #include <iostream> 
-
+#include <unordered_map> //included for private data member adjList 
+using namespace std; 
 class MyGraph{ //declaration of class MyGraph 
     public: //public members accessible outside of MyGraph class 
     
 
     private: //private members only accessible to MyGraph class 
-        MyGraph(int n){//Create a graph with n vertices. The vertices are labelled 1..n 
-            
+        //define the adjList private data member as an unordered map. 
+        //This data member maps each vertex to a vector of pairs, where each pair consists of a neighboring vertex & the weight of the edge that connects them 
+        unordered_map<int, vector<pair<int, float>>> adjList; 
+       
+        MyGraph(int n){//Create a graph with n vertices. The vertices are labelled 1..n. Can use adjacency list or adjacency matrix. 
+            for(int i = 1; i < n; i++) //loop through all your n values 
+            {
+                addVertex(i); //call the addVertex function 
+            }
+        }
+
+        void addVertex(int v){//i (the current vertice) is passed into add vertex as V 
+            if (adjList.find(v) == adjList.end()){//if the vertex is not in the adjList private data member then add 
+                adjList[v] = vector<pair<int, float>>{}; //add the vertex to adjList data member 
+            }
         }
 
         MyGraph(const MyGraph& g){//Construct a new graph that is a copy of g 
