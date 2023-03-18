a94c681ab54b729a86d534a630e73e0fb90d3118
MaxDLink <max.durham.link@gmail.com>
2023-03-18 13:27:02 -0500

    python code -- generates # of verticies for tests

---
+++ gen.py
@@ -0,0 +1,37 @@
+import random
+
+def generate_edges(n, num_edges):
+    edges = []
+    for i in range(1, n):
+        edges.append((i, i + 1, random.randint(1, 100)))
+    remaining_edges = num_edges - (n - 1)
+    for _ in range(remaining_edges):
+        a, b = random.sample(range(1, n + 1), 2)
+        weight = random.randint(1, 100)
+        edges.append((a, b, weight))
+    return edges
+
+def generate_queries(num_queries, n):
+    queries = []
+    for _ in range(num_queries):
+        a, b = random.sample(range(1, n + 1), 2)
+        queries.append((a, b))
+    return queries
+
+def generate_dataset(n, num_edges, num_queries):
+    edges = generate_edges(n, num_edges)
+    queries = generate_queries(num_queries, n)
+
+    with open('dataset.txt', 'w') as f:
+        f.write(f'{n} {num_edges} {num_queries}\n')
+        for edge in edges:
+            f.write(f'{edge[0]} {edge[1]} {edge[2]}\n')
+        for query in queries:
+            f.write(f'{query[0]} {query[1]}\n')
+
+if __name__ == '__main__':
+    n = 1000
+    num_edges = 1500  # Adjust the number of edges as needed
+    num_queries = 20
+    generate_dataset(n, num_edges, num_queries)
+
